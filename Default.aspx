5829c84d4f98f076a5caf16806aad839ebcaabcd
atimmer01 <atimmer@mirabeau.nl>
2017-03-10 09:42:27 +0100

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SubmitDeploymentToNewRelic.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="/css/all.css">
+</head>
+<body>
+    
+    <header class="main-header">
+        <a href="#" class="logo">Mirabeau</a>
+    </header>
+    <form id="form1" runat="server" class="main-header">
+    <div>
+        <asp:Label ID="lblApplication" runat="server" Text="Application" Font-Bold="true" Font-Size="Medium"></asp:Label><br />
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" style="height: 30px; width: 400px">
+        </asp:DropDownList><br />
+        <asp:XmlDataSource ID="ClientsDataSource" runat="server" DataFile="~/App_Data/Clients.xml"></asp:XmlDataSource>
+        <br />
+        <asp:Label ID="lblNewRelicApiKey" runat="server" Text="NewRelic apiKey" Font-Bold="true" Font-Size="Medium"></asp:Label><br />
+        <asp:TextBox ID="txtNewRelicApiKey" runat="server" Width="400px" Height="30px"></asp:TextBox><br />
+        <br />
+        <asp:Label ID="lblNewRelicAppName" runat="server" Text="NewRelic ApplicationId" Font-Bold="true" Font-Size="Medium"></asp:Label><br />
+        <asp:TextBox ID="txtNewRelicAppName" runat="server" Width="400px" Height="30px"></asp:TextBox><br /><br />
+        <asp:Label ID="lblNewRelicUser" runat="server" Text="NewRelic Deployment User" Font-Bold="true" Font-Size="Medium"></asp:Label><br />
+        <asp:TextBox ID="txtNewRelicUser" runat="server" Width="400px" Height="30px"></asp:TextBox><br />
+        <asp:RequiredFieldValidator runat="server" id="reqUser" controltovalidate="txtNewRelicUser" errormessage="Vul je naam in!" Font-Bold="false" Font-Size="Medium" ForeColor="Red"/>
+        <br /><br />
+        <asp:Label ID="lblNewRelicDeploymentRevision" runat="server" Text="NewRelic Deployment Revision" Font-Bold="true" Font-Size="Medium"></asp:Label><br />
+        <asp:TextBox ID="txtNewRelicDeploymentRevision" runat="server" Height="80px" TextMode="MultiLine" Width="400px"></asp:TextBox><br />
+        <asp:RequiredFieldValidator runat="server" id="reqRevision" controltovalidate="txtNewRelicDeploymentRevision" errormessage="Vul een release / deployment omschrijving in!" Font-Bold="false" Font-Size="Medium" ForeColor="Red"/>
+        <br /><br />
+        <asp:Button ID="btnSubmitNewRelicDeployment" runat="server" Text="Submit deployment to NewRelic" OnClick="btnSubmitNewRelicDeployment_Click" Width="400px" Height="60px" Font-Size="Medium" /><br />
+        <br />
+        <asp:Label ID="lblError" runat="server" Text="" Font-Bold="true" Font-Size="Medium"></asp:Label>
+        <asp:Label ID="lblSuccess" runat="server" Text="" Font-Bold="true" Font-Size="Medium"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Xml;
+using System.Net;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace SubmitDeploymentToNewRelic
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        XmlDocument Clients = new XmlDocument();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblError.ForeColor = System.Drawing.Color.Red;
+            lblSuccess.ForeColor = System.Drawing.Color.Green;
+
+            Clients.Load(Server.MapPath("~/App_Data/Clients.xml"));
+
+            if (!Page.IsPostBack) // to avoid reloading your control on postback
+            {
+                XmlNodeList clientList = Clients.SelectNodes("/clients/client/Name");
+                foreach (XmlNode Name in clientList)
+                {
+                    DropDownList1.Items.Add(Name.InnerText);
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = DropDownList1.SelectedIndex;
+
+            XmlNodeList nodelist = Clients.SelectNodes("/clients/client");
+            XmlNode xmlnode = nodelist.Item(selectedIndex);
+
+            txtNewRelicApiKey.Text = xmlnode.SelectSingleNode("apiKey").InnerText;
+            txtNewRelicAppName.Text = xmlnode.SelectSingleNode("applicationId").InnerText;
+
+        }
+
+        protected void btnSubmitNewRelicDeployment_Click(object sender, EventArgs e)
+        {
+            string apiUrl = "https://s2events.azure-automation.net/webhooks?token=iPgtnBmbB1n2MQJ5cwqzzgztOcPtvyBpNmySDFWNVaQ%3d";
+
+            try
+            {
+                string apiKey = txtNewRelicApiKey.Text;
+                string applicationId = txtNewRelicAppName.Text;
+                string revision = txtNewRelicDeploymentRevision.Text;
+                string user = txtNewRelicUser.Text;
+
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(apiUrl);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = @"{'apiKey':'" + apiKey + "','applicationId':'" + applicationId + "','revision':'" + revision + "','user':'" + user + "'}";
+
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
+
+                lblSuccess.Text = "Succesfully submitted deployment to NewRelic!";
+            }
+            catch (Exception)
+            {
+
+                Exception ex = new Exception();
+                lblError.Text = ex.Message.ToString();
+            }
+            finally
+            {
+                
+            }
+        }
+    }
+}
